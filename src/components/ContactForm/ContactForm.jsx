a7bdc718fb12b0dd79f8b9bd01fc796efc9cedde
IvaZach <ivazacherniuk@gmail.com>
2023-12-18 11:22:09 +0300

    Add Formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,76 +1,87 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import { nanoid } from 'nanoid';
 import css from './ContactForm.module.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  handleChange = evt => {
-    const { name, value } = evt.currentTarget;
-    this.setState({ [name]: value });
-  };
+const initialValues = {
+  name: '',
+  number: '',
+};
 
-  handleSubmit = e => {
-    e.preventDefault();
+let schema = yup.object().shape({
+  name: yup
+    .string()
+    .trim()
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d`Artagnan'
+    )
+    .required(),
+  number: yup
+    .string()
+    .trim()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required(),
+});
 
+export const ContactForm = ({ onSubmit }) => {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    const { name, number } = values;
     const contact = {
-      name: this.state.name,
-      number: this.state.number,
+      name: name.trim(),
+      number: number.trim(),
       id: nanoid(6),
     };
-
-    this.props.onSubmit(contact);
-    this.reset();
+    onSubmit(contact);
+    resetForm();
   };
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={css.form}>
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={css.form}>
         <div className="mb-3">
           <label htmlFor="validationDefault01" className="form-label">
             Name
           </label>
-          <input
+          <Field
             type="text"
             name="name"
             id="validationDefault01"
-            value={this.state.name}
-            onChange={this.handleChange}
-            required
             className="form-control"
             placeholder="first and last name"
           />
+          <ErrorMessage name="name" component="div" />
         </div>
         <div className="mb-3">
           <label htmlFor="inputTel" className="form-label">
             Number
           </label>
-          <input
+          <Field
             type="tel"
             name="number"
             id="inputTel"
-            value={this.state.number}
-            onChange={this.handleChange}
-            required
             className="form-control"
             placeholder="###-##-##"
-            pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
           />
+          <ErrorMessage name="number" component="div" />
         </div>
 
         <button type="submit" className="btn btn-primary">
           Add contact
         </button>
-      </form>
-    );
-  }
-}
+      </Form>
+    </Formik>
+  );
+};
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
